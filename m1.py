35ecf094e63cdda84d739871a907fe06b07c9511
lxooboatoxl1 <lattaphonh9998@gmail.com>
2023-10-18 14:46:36 +1100

    All mutants are added

--- m1.py
+++ m1.py
@@ -1,7 +1,9 @@
+#M1 line6 : add radius instead of multiply the radius
 import math
 
 def calculate_area_of_circle(radius):
-    return math.pi * radius * radius
+#p :return math.pi * radius * radius
+    return math.pi * radius + radius
 
 radius1, radius2 = map(int, input("Enter the radius of the circles separated by a space: ").split())
 if radius1 == 0 and radius2 == 0:
@@ -10,3 +12,5 @@ else:
     area1 = calculate_area_of_circle(radius1)
     area2 = calculate_area_of_circle(radius2)
     print("%.2f" % area1, "%.2f" % area2)
+
+
---
+++ m20.py
@@ -0,0 +1,14 @@
+import math
+
+def calculate_area_of_circle(radius):
+    return math.pi * radius * radius
+
+radius1, radius2 = map(int, input("Enter the radius of the circles separated by a space: ").split())
+if radius1 == 0 and radius2 == 0:
+    print("Radius cannot be 0. Program stopped.")
+else:
+    area1 = calculate_area_of_circle(radius1)
+    area2 = calculate_area_of_circle(radius2)
+    print("%.2f" % area1, "%.2f" % area2)
+
+
